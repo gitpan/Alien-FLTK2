60e31826396497bbf1390d009ff05de6a6a9163a
Sanko Robinson <sanko@cpan.org>
2011-02-08 03:49:29 +0000

    Import of SANKO/Alien-FLTK2-0.08365 from CPAN.
    gitpan-cpan-distribution: Alien-FLTK2
    gitpan-cpan-version:      0.08365
    gitpan-cpan-path:         SANKO/Alien-FLTK2-0.08365.tar.gz
    gitpan-cpan-author:       SANKO
    gitpan-cpan-maturity:     released

--- lib/Alien/FLTK2.pm
+++ lib/Alien/FLTK2.pm
@@ -3,13 +3,14 @@ package Alien::FLTK2;
     use strict;
     use warnings;
     use File::Spec::Functions qw[catdir rel2abs canonpath];
-    our $BASE = 0; our $SVN = 8323; our $DEV = -1; our $VERSION = sprintf('%d.%05d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $BASE, $SVN, abs $DEV);
+    our $BASE = 0; our $SVN = 8365; our $DEV = 0; our $VERSION = sprintf('%d.%05d' . ($DEV ? (($DEV < 0 ? '' : '_') . '%03d') : ('')), $BASE, $SVN, abs $DEV);
 
     sub _md5 {
-        return {bz2 => 'd3dc8494c73bc3f0a7f0fed5a7ca0a8e',
-                gz  => 'b735071bf44398e1b8f8c1d9dad7ac30'
+        return {bz2 => 'a4a78694b8fc0e602cbc1c2a44267847',
+                gz  => 'f2c1144be28dadb0d2ef427f3392c4b5'
         };
     }
+
     sub _unique_file { return 'src/Widget.cxx' }
 
     sub new {
@@ -404,6 +405,6 @@ clarification, see http://creativecommons.org/licenses/by-sa/3.0/us/.
 L<Alien::FLTK2|Alien::FLTK2> is based in part on the work of the FLTK project.
 See http://www.fltk.org/.
 
-=for git $Id: FLTK2.pm 8088b1a 2011-02-02 23:58:04Z sanko@cpan.org $
+=for git $Id: FLTK2.pm ec4e842 2011-02-08 03:43:36Z sanko@cpan.org $
 
 =cut
